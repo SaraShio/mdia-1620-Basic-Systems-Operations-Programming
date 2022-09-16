4dcb19c27d450dd929be970d603c64295a9fdd80
Sara Shiojima <sshiojima@se14l0120-16.ad.bcit.ca>
2022-09-16 14:45:29 -0700

    new code

---
+++ Part-1/Week2/test.js
@@ -0,0 +1,11 @@
+// Declare
+// Declare a variable called electronic devices
+// camel case?
+var electronicDevies;
+
+// pascal case?
+var ElectronicDevices;
+
+// snake caase?
+var Electronic_Devices;
+
